7a1344ba43bad95cf1dab29dde3c743e697d8ab1
Dan Beavin <dbeavin@vigilantnow.com>
2026-03-25 12:18:13 -0400

    Add fwknop formula

---
+++ Formula/fwknop.rb
@@ -0,0 +1,23 @@
+class Fwknop < Formula
+  desc "Single Packet Authorization and port knocking"
+  homepage "https://www.cipherdyne.org/fwknop/"
+  url "https://github.com/mrash/fwknop/archive/refs/tags/2.6.11.tar.gz"
+  sha256 "b90c09982d82d9e197477f24dc49d07b362d0844f1bbfd0f68efae72e70db102"
+  license "GPL-2.0-or-later"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  def install
+    system "autoreconf", "--install", "--force", "--verbose"
+    system "./configure", *std_configure_args,
+                          "--disable-server",
+                          "--with-gpgme=no"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/fwknop", "--version"
+  end
+end
