88b20a75ff1d0d19d56df5cb65dd6e700159c525
dagecko <cnyhuis@vigilantnow.com>
2026-03-28 17:24:43 -0400

    Update runner-guard to v2.4.7

--- Formula/runner-guard.rb
+++ Formula/runner-guard.rb
@@ -10,14 +10,14 @@ class RunnerGuard < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/Vigilant-LLC/runner-guard/releases/download/v2.4.7/runner-guard_2.4.7_darwin_amd64.tar.gz"
-      sha256 "e8743dc6104d2fc5070cf35469a9e664c2df5c894a1776a02ff0e157d421134b"
+      sha256 "0c601ccb07f0cf0c9f603b5794b284c0caa4f0e89c8d0784a0ab49dc746b5aa3"
       define_method(:install) do
         bin.install "runner-guard"
       end
     end
     if Hardware::CPU.arm?
       url "https://github.com/Vigilant-LLC/runner-guard/releases/download/v2.4.7/runner-guard_2.4.7_darwin_arm64.tar.gz"
-      sha256 "5ed6e190b134e229c04e8ec1951c4090f4d89328db45564b4005e98e47793e51"
+      sha256 "2795ecbee67fcd79786b72c1e761edfdc2f792158da8a0485c18812f97b6c508"
       define_method(:install) do
         bin.install "runner-guard"
       end
@@ -27,14 +27,14 @@ class RunnerGuard < Formula
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/Vigilant-LLC/runner-guard/releases/download/v2.4.7/runner-guard_2.4.7_linux_amd64.tar.gz"
-      sha256 "e091db914ca7ba16a6353a2fcfb718d41bc744771bf23b9ba3b630d129b0dae0"
+      sha256 "30ba533588c444f1b18d853afafb884d417c13aa941b3ceeea56aa5d698895fe"
       define_method(:install) do
         bin.install "runner-guard"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/Vigilant-LLC/runner-guard/releases/download/v2.4.7/runner-guard_2.4.7_linux_arm64.tar.gz"
-      sha256 "9900291e7b3e850e5faa9f450a61e33612ec1d448f1e476470e7bdc889f9e3ff"
+      sha256 "4d32495c4c0195f1ae290f5aeca632ec41ad57d08ee2b725a2d28ac680c77c43"
       define_method(:install) do
         bin.install "runner-guard"
       end
