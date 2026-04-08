2e7330ef6005288f9c2668c4cd3ccbe729c72199
dagecko <cnyhuis@vigilantnow.com>
2026-04-08 07:41:53 -0400

    Update runner-guard v3.0.1 checksums (trimpath rebuild)

--- Formula/runner-guard.rb
+++ Formula/runner-guard.rb
@@ -10,7 +10,7 @@ class RunnerGuard < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/Vigilant-LLC/runner-guard/releases/download/v3.0.1/runner-guard_3.0.1_darwin_amd64.tar.gz"
-      sha256 "10d6a6d67fe15a9cbe96deedf5b5b7c812bc108280570f311f68e30387fd6041"
+      sha256 "93ea7a7b6d1fadd59f2a52caaceba837e361ede9e0f739a9ea9d3aca242d4c2a"
 
       define_method(:install) do
         bin.install "runner-guard-darwin-amd64" => "runner-guard"
@@ -18,7 +18,7 @@ class RunnerGuard < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/Vigilant-LLC/runner-guard/releases/download/v3.0.1/runner-guard_3.0.1_darwin_arm64.tar.gz"
-      sha256 "9777f88074eec869d182db597db2799f0489cccc1cefe5ad5cd4b9ac4854772f"
+      sha256 "02469c557ea07e2f1751ae46ebbc7da1ee62c4bc86e6b673a4df93f27033e335"
 
       define_method(:install) do
         bin.install "runner-guard-darwin-arm64" => "runner-guard"
@@ -29,14 +29,14 @@ class RunnerGuard < Formula
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/Vigilant-LLC/runner-guard/releases/download/v3.0.1/runner-guard_3.0.1_linux_amd64.tar.gz"
-      sha256 "991d0d09de1c28f00db0592cef16b66bdaa9bba779a7fbcf2e32ba4ed8a5cd5d"
+      sha256 "483bbf4a1bbb92d8b9a3b26977901f567c8fe0ca6d5f29a885f9b5d04f1af9b7"
       define_method(:install) do
         bin.install "runner-guard-linux-amd64" => "runner-guard"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/Vigilant-LLC/runner-guard/releases/download/v3.0.1/runner-guard_3.0.1_linux_arm64.tar.gz"
-      sha256 "55910001fe8f4bee12ac987ed00b4a72a54f1d40e357661458195787e81accc0"
+      sha256 "8f6ed34c00dfc49b7122bee4e20edacbea7771bdc5d1a77322e473a742f42189"
       define_method(:install) do
         bin.install "runner-guard-linux-arm64" => "runner-guard"
       end
